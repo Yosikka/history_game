5cad5ac5f24f973f5f89bf981d40c32c24bacfb7
Yosikka <161472478+Yosikka@users.noreply.github.com>
2025-12-30 19:03:09 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-    console.log("Server running");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on", PORT);
 });
